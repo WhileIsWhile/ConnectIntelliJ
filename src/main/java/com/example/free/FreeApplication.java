61f9c32c558909efe62e0f835616d9c2e2c483ca
wothd0406 <wothd0406@gmail.com>
2023-08-30 09:46:57 +0900

    free commit

---
+++ src/main/java/com/example/free/FreeApplication.java
@@ -0,0 +1,13 @@
+package com.example.free;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FreeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/free/FreeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.free;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
